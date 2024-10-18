f350b061f76418994ff89c1dd83e08d4eb344b63
Jareem Hoff <jareem.e.hoff@gmail.com>
2024-10-17 22:57:58 -0400

    will spend more time trying to clean this up

--- src/auth_handlers/auth.ts
+++ src/auth_handlers/auth.ts
@@ -1,13 +1,68 @@
-export const create_account = async (event) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      event: event,
-      test_message: "test message",
-    }),
-  };
+import { Amplify, ResourcesConfig } from "aws-amplify";
+import { signUp } from "aws-amplify/auth";
 
-  console.log("testing everythin here");
 
-  return response;
+const header = new Headers({
+  "Content-Type": "application/json",
+  "Access-Control-Allow-Origin": "*",
+});
+
+const aws_config: ResourcesConfig = {
+  Auth: {
+    Cognito: {
+      userPoolId: 'us-east-1_dKqcnhrFn',
+      userPoolClientId: '2u3pvu6f0hmank1885h03od38k',
+      signUpVerificationMethod: 'code',
+    },
+  },
 };
+
+Amplify.configure(aws_config);
+
+export const create_account = async (event) => {
+  const _user_info = event;
+  console.log("user_info: ", _user_info);
+  try {
+    const user = await signUp({
+      username: _user_info.username,
+      password: _user_info.password,
+      options: {
+        userAttributes: {
+          email: _user_info.email,
+          // first_name: _user_info.first_name,
+          // last_name: _user_info.last_name,
+        },
+      }
+    }).then((data) => {
+      
+    });
+
+    const response = {
+      statusCode: 200,
+      headers: header,
+      body: event,
+      data: user
+    };
+    // return user;
+
+    return response;
+  } catch (error) {
+
+    const response = {
+      statusCode: 500,
+      headers: header,
+      body: event,
+      error: error
+    };
+
+    return response;
+  }
+};
+
+function create_body(_event) {
+  const _body = JSON.stringify({
+    event: _event.body,
+  })
+
+  return _body;
+}
